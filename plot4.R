dd57b23e7c58c6204ecb9451a39257f94d6def6f
KittyKuong <36538874+KittyKuong@users.noreply.github.com>
2018-04-30 07:15:44 +0800

    Add files via upload

---
+++ plot4.R
@@ -0,0 +1,27 @@
+data<- read.table("household_power_consumption.txt", skip=1,sep=";")
+names(data) <- c("Date", "Time", "Global_active_power","Global_reactive_power", "Voltage", "Global_intensity","Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+subdata <-subset(data, data$Date == "1/2/2007" | data$Date == "2/2/2007")
+
+daterange <- strptime(paste(subdata$Date, subdata$Time, sep = " "), "%d/%m/%Y %H:%M:%S")
+GAPdata <- as.numeric(subdata$Global_active_power)
+GRPdata <- as.numeric(subdata$Global_reactive_power)
+voltdata <- as.numeric(subdata$Voltage)
+metering1 <- as.numeric(subdata$Sub_metering_1)
+metering2 <- as.numeric(subdata$Sub_metering_2)
+metering3 <- as.numeric(subdata$Sub_metering_3)
+
+png("plot4.png", width=480,height=480)
+par(mfrow = c(2,2))
+
+plot(daterange, GAPdata, type ="l", ylab = "Global Active Power", cex =0.2)
+
+plot(daterange, voltdata, type ="l", xlab= "datetime", ylab = "Voltage")
+
+plot(daterange, metering1, type="l", ylab = "Energy Submetering")
+lines(daterange, metering2, type ="l", col="red")
+lines(daterange, metering3, type ="l",col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+
+plot(daterange, GRPdata, type ="l", ylab = "Global_reactive_power")
+
+dev.off()
